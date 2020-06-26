ccc1527f9bbf13c7f080ffb0e267f8db5e9c49ae
Mr Robot <davidallandede@hotmail.com>
2020-06-26 08:53:04 -0300

    Criado um novo modelo para categorizar a origem do livro

---
+++ CRUD_01/BookStore/BookStore.Ui/Models/Origem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Ui.Models
+{
+    public class Origem : Entity
+    {
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
