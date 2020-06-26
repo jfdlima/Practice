af26582c5c884c1287f5d64b38e5ad2cd4328a1b
Mr Robot <davidallandede@hotmail.com>
2020-06-26 08:52:23 -0300

    Criado uma entidade para os atributos semelhantes

---
+++ CRUD_01/BookStore/BookStore.Ui/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Ui.Models
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
